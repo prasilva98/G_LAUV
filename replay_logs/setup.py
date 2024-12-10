a7685f6afd38e669acf312c515d7dcc61cbbdb47
Ruben Silva <paulo.ras98@gmail.com>
2024-12-10 12:06:43 +0000

    added launch files and udpated setup to include them

--- replay_logs/setup.py
+++ replay_logs/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os 
+from glob import glob
 
 package_name = 'replay_logs'
 
@@ -7,9 +9,19 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+      # Include resource idnex
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
+        # Include package.xml
         ('share/' + package_name, ['package.xml']),
+
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        
+        # Include config files
+        (os.path.join('share', package_name, 'config'), ['config/ros_gazebo_topics.yaml']),
+
     ],
     install_requires=['setuptools', 'pandas'],
     zip_safe=True,
